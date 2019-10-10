84fec9dd5e711d56ebf3abcb36e71d56d9e74a0d
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2019-10-10 15:53:02 +0200

    Add randomPopulate method

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Matrice m1 = new Matrice(2,4,5);
-
+        System.out.println(m1);
     }
 
 }
--- src/Matrice.java
+++ src/Matrice.java
@@ -1,8 +1,9 @@
 public class Matrice {
 
-    int n;
-    int m;
-    int modulo;
+    private int n;
+    private int m;
+    private int modulo;
+    private int[][] content;
 
     public Matrice(int n, int m, int modulo){
         // The matrix size n and m must be between 0 and modulo - 1
@@ -15,18 +16,21 @@ public class Matrice {
         this.n = n;
         this.m = m;
         this.modulo = modulo;
+        this.content = new int[n][m];
 
         randomPopulate();
     }
 
     private void randomPopulate(){
-        /*
-        for (int row = 0; row < this.n; ++row) {
-            for(int col = 0; col < this.m; ++row){
+        int max = modulo - 1;
+        int min = 0;
+        int range = max - min + 1;
 
+        for (int row = 0; row < this.n; ++row) {
+            for(int col = 0; col < this.m; ++col){
+                content[row][col] = (int)(Math.random() * range) + min;
             }
         }
-        */
     }
 
     @Override
