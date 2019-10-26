2f1bc8f070828113fac0cae03d5af7d8af89002b
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2019-10-26 14:35:13 +0200

    Finish lab

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Matrice m1 = new Matrice(2,4,5);
-        Matrice m2 = new Matrice(2,4,5);
+        int modulus = 5;
 
-        Matrice m3 = m1.add(m2);
+        Matrice m1 = new Matrice(new int[][] {{1,3,1,1},{3,2,4,2},{1,0,1,0}}, modulus);
+        Matrice m2 = new Matrice(new int[][] {{1,4,2,3,2},{0,1,0,4,2},{0,0,2,0,2}}, modulus);
 
+        System.out.println("The modulus is " + modulus + "\n");
+
+        System.out.println("one");
         System.out.println(m1);
+
+        System.out.println("two");
         System.out.println(m2);
-        System.out.println(m3);
+
+        System.out.println("one + two");
+        System.out.println(m1.add(m2));
+
+        System.out.println("one - two");
+        System.out.println(m1.sub(m2));
+
+        System.out.println("one x two");
+        System.out.println(m1.multiply(m2));
     }
 
 }
--- src/Matrice.java
+++ src/Matrice.java
@@ -2,31 +2,51 @@ public class Matrice {
 
     private int n;
     private int m;
-    private int modulo;
+    private int modulus;
     private int[][] content;
 
-    public Matrice(int n, int m, int modulo) {
-        // The matrix size n and m must be between 0 and modulo - 1
-        if (modulo == 0) {
-            throw new RuntimeException("The matrix size (n and m) must be between 0 and modulo - 1");
-        }
+    public Matrice(int n, int m, int modulus) {
+        testModulus(modulus);
+        testMatrixDimension(n,m);
 
         this.n = n;
         this.m = m;
-        this.modulo = modulo;
+        this.modulus = modulus;
         this.content = new int[n][m];
 
         randomPopulate();
     }
 
+    public Matrice(int[][] values, int modulus) {
+        testModulus(modulus);
+        testMatrixDimension(values.length,values[0].length);
 
+        // Test if each matrix rows have the same length
+        int rowLength = values[0].length;
+        for (int[] row : values) {
+            if(rowLength != row.length){
+                throw new RuntimeException("All the matrix rows must have the same length");
+            }
+        }
 
-    // TODO : Faire un constructeur qui prend un tableau en parametre pour ainsi pouvoir créer une matrice avec exactement les valeurs qu'on veut
+        // Test if each matrix values are between 0 and modulus - 1
+        for(int row = 0; row < values.length; ++row){
+            for(int col = 0; col < values[row].length; ++col){
+                if(values[row][col] < 0 || values[row][col] > (modulus-1)){
+                    throw new RuntimeException("All the matrix values must be between 0 and modulus - 1");
+                }
+            }
+        }
 
+        this.n = values.length;
+        this.m = values[0].length;
+        this.modulus = modulus;
+        this.content = values;
+    }
 
 
     private void randomPopulate(){
-        int max = modulo - 1;
+        int max = modulus - 1;
         int min = 0;
         int range = max - min + 1;
 
@@ -35,28 +55,6 @@ public class Matrice {
                 content[row][col] = (int)(Math.random() * range) + min;
             }
         }
-    }
-
-    private Matrice doOperation(Matrice other, Operation operation){
-        if (this.modulo != other.modulo) {
-            throw new RuntimeException();
-        }
-
-        int maxN = Math.max(this.n, other.n);
-        int maxM = Math.max(this.m, other.m);
-
-        Matrice sum = new Matrice(maxN, maxM, this.modulo);
-
-        for (int row = 0; row < maxN; ++row) {
-            for(int col = 0; col < maxM; ++col){
-                int op1 = row > this.n || col > this.m ? 0 : this.content[row][col];
-                int op2 = row > other.n || col > other.m ? 0 : other.content[row][col];
-                int operationResult = operation.calculate(op1, op2);
-                sum.content[row][col] = operationResult % sum.modulo;
-            }
-        }
-
-        return sum;
     }
 
     public Matrice add(Matrice other) {
@@ -82,5 +80,42 @@ public class Matrice {
         }
 
         return str;
+    }
+
+    private void testModulus(int modulus){
+        // The matrix values must be between 0 and modulus - 1, so we cannot have a modulus of 0
+        if (modulus == 0) {
+            throw new RuntimeException("We cannot have a modulus of 0 because the matrix values must be between 0 and modulus - 1");
+        }
+    }
+
+    private void testMatrixDimension(int n, int m) {
+        if(n == 0 || m == 0){
+            throw new RuntimeException("Matrix cannot be void");
+        }
+    }
+
+    private Matrice doOperation(Matrice other, Operation operation){
+        if (this.modulus != other.modulus) {
+            throw new RuntimeException("Both matrix must have the same modulus");
+        }
+
+        int maxN = Math.max(this.n, other.n);
+        int maxM = Math.max(this.m, other.m);
+
+        Matrice sum = new Matrice(maxN, maxM, this.modulus);
+
+        for (int row = 0; row < maxN; ++row) {
+            for(int col = 0; col < maxM; ++col){
+                // Complete with 0 value if matrix have not the same rows or columns
+                int op1 = row >= this.n || col >= this.m ? 0 : this.content[row][col];
+                int op2 = row >= other.n || col >= other.m ? 0 : other.content[row][col];
+                int operationResult = operation.calculate(op1, op2);
+                // We used Math.floorMod instead of % to have the good result with negative numbers
+                sum.content[row][col] = Math.floorMod(operationResult,sum.modulus);
+            }
+        }
+
+        return sum;
     }
 }
