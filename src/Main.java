f75d7b506b057accd9e3ba04a02bb0f0bb47b213
Doran Kayoumi <dorankayoumi@gmail.com>
2019-10-29 18:03:16 +0100

    Add code to test breaking points

--- src/Main.java
+++ src/Main.java
@@ -13,30 +13,49 @@ public class Main {
 
         test(3, 4, 3, 5, 5);
 
-        // TODO : Remove les tests ci-dessous qui servent juste d'example pour voir que ca marche comme l'example du prof
-        /*
-        int modulus = 5;
+        testBreakingPoint();
+    }
 
-        Matrix m1 = new Matrix(new int[][] {{1,3,1,1},{3,2,4,2},{1,0,1,0}}, modulus);
-        Matrix m2 = new Matrix(new int[][] {{1,4,2,3,2},{0,1,0,4,2},{0,0,2,0,2}}, modulus);
+    /**
+     * Test all the breaking points of the Matrix class
+     * i.e. when exceptions are lifted
+     */
+    private static void testBreakingPoint() {
+        System.out.println("Testing breaking points");
+        try {
+            System.out.println("Creating a Matrix with an invalid modulus:");
+            // creating a matrix w/ an invalid modulus
+            new Matrix(2, 3, -1);
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+        }
 
-        System.out.println("The modulus is " + modulus + "\n");
+        System.out.println();
+        try {
+            System.out.println("Creating a Matrix with invalid dimensions:");
+            new Matrix(-2, 3, 3);
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+        }
 
-        System.out.println("one");
-        System.out.println(m1);
+        System.out.println();
+        try {
+            System.out.println("Creating a Matrix with data not having the same amount of rows and columns:");
+            new Matrix(new int[][] {{1,3,1},{3,2,4,2},{1,0,1,0}}, 3);
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+        }
 
-        System.out.println("two");
-        System.out.println(m2);
+        System.out.println();
+        try {
+            System.out.println("Performing an operation on matrices with different modulus:");
+            Matrix m1 = new Matrix(3, 3, 4);
+            Matrix m2 = new Matrix(3, 3, 5);
 
-        System.out.println("one + two");
-        System.out.println(m1.add(m2));
-
-        System.out.println("one - two");
-        System.out.println(m1.sub(m2));
-
-        System.out.println("one x two");
-        System.out.println(m1.multiply(m2));
-        */
+            m1.add(m2);
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     /**
@@ -52,7 +71,7 @@ public class Main {
         Matrix matrix1 = new Matrix(n1, m1, modulus);
         Matrix matrix2 = new Matrix(n2, m2, modulus);
 
-        System.out.println("The modulus is " + modulus + "\n");
+        System.out.println("The modulus is " + modulus);
 
         System.out.println("one");
         System.out.println(matrix1);
